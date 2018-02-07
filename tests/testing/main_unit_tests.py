7dcc4a62037bf8c300c3f7d3833e8322152f7b7c
evandrocoan <evandrocoan@hotmail.com>
2018-02-07 02:14:37 -0200

    Reimplemented the sys.stderr logging capture for main_unit_tests.py

--- tests/testing/main_unit_tests.py
+++ tests/testing/main_unit_tests.py
@@ -46,6 +46,58 @@ new_err = StringIO()
 old_err = sys.stderr
 
 
+class TeeNoFile(object):
+    """
+        How do I duplicate sys.stdout to a log file in python?
+        https://stackoverflow.com/questions/616645/how-do-i-duplicate-sys-stdout-to-a-log-file-in-python
+    """
+
+    def __init__(self):
+        self._contents = []
+
+        self._stderr = sys.stderr
+        sys.stderr = self
+
+    def __del__(self):
+        self.close()
+
+    def clear(self):
+        self._contents.clear()
+
+    def flush(self):
+        self._stderr.flush()
+
+    def write(self, data):
+        self._stderr.write( data )
+        self._contents.append( data )
+
+    def contents(self, module_name, base_date):
+        clean_output = []
+
+        base_date_length = len( base_date )
+        module_name_length = len( module_name )
+
+        output = "".join( self._contents )
+        output = output.strip().split( "\n" )
+
+        for line in output:
+            clean_output.append( line[:module_name_length] + line[module_name_length+base_date_length:] )
+
+        return "\n".join( clean_output )
+
+    def close(self):
+        self.clear()
+
+        if self._stderr is not None:
+            sys.stderr = self._stderr
+            self._stderr = None
+
+
+# We need to keep a global reference to this because the logging module internally grabs an
+# reference to the first `sys.strerr` it can get its hands on it.
+stderr = TeeNoFile()
+
+
 class MainUnitTests(unittest.TestCase):
     """
         How to assert output with nosetest/unittest in python?
@@ -59,170 +111,136 @@ class MainUnitTests(unittest.TestCase):
         """
         self.maxDiff = None
 
-    def tearDown(self):
-        """
-            Explain the “setUp” and “tearDown” Python methods used in test cases
-            https://stackoverflow.com/questions/6854658/explain-the-setup-and-teardown-python-methods-used-in-test-cases
-        """
-        new_err.seek(0)
-        new_err.truncate(0)
-
-    def getOutput(self, module_name, base_date):
-        clean_output = []
-        output = new_err.getvalue().strip().split( "\n" )
-
-        base_date_length = len( base_date )
-        module_name_length = len( module_name )
-
-        for line in output:
-            clean_output.append( line[:module_name_length] + line[module_name_length+base_date_length:] )
-
-        output = "\n".join( clean_output )
-        return output
-
     def test_function_name(self):
+        stderr.clear()
+        log = getLogger( 127, "testing.main_unit_tests", date=True, function=True )
 
-        try:
-            sys.stderr = new_err
-            log = getLogger( 127, "testing.main_unit_tests", date=True, function=True )
+        log( 1, "Bitwise" )
+        log( 8, "Bitwise" )
+        log.warn( "Warn" )
+        log.info( "Info" )
+        log.debug( "Debug" )
+        log.newline()
 
+        def function_name():
             log( 1, "Bitwise" )
             log( 8, "Bitwise" )
             log.warn( "Warn" )
             log.info( "Info" )
             log.debug( "Debug" )
-
-            def function_name():
-                log( 1, "Bitwise" )
-                log( 8, "Bitwise" )
-                log.warn( "Warn" )
-                log.info( "Info" )
-                log.debug( "Debug" )
-
             log.newline()
-            function_name()
-            log.reset()
 
-        finally:
-            sys.stderr = old_err
+        function_name()
+        log.reset()
 
-        output = self.getOutput( "[testing.main_unit_tests] ", "2018-01-28 20:29:26:617.002010 3.63e-04 " )
+        output = stderr.contents( "[testing.main_unit_tests] ", "2018-01-28 20:29:26:617.002010 3.63e-04 " )
 
         if is_python2:
             self.assertEqual( wrap_text( """\
-                    [testing.main_unit_tests] (unknown function):0 Bitwise
-                    [testing.main_unit_tests] (unknown function):0 Bitwise
-                    [testing.main_unit_tests] (unknown function):0 Warn
-                    [testing.main_unit_tests] (unknown function):0 Info
-                    [testing.main_unit_tests] (unknown function):0 Debug
+                [testing.main_unit_tests] (unknown function):0 Bitwise
+                [testing.main_unit_tests] (unknown function):0 Bitwise
+                [testing.main_unit_tests] (unknown function):0 Warn
+                [testing.main_unit_tests] (unknown function):0 Info
+                [testing.main_unit_tests] (unknown function):0 Debug
 
-                    [testing.main_unit_tests] (unknown function):0 Bitwise
-                    [testing.main_unit_tests] (unknown function):0 Bitwise
-                    [testing.main_unit_tests] (unknown function):0 Warn
-                    [testing.main_unit_tests] (unknown function):0 Info
-                    [testing.main_unit_tests] (unknown function):0 Debug
-                    """ ),
-                    output )
+                [testing.main_unit_tests] (unknown function):0 Bitwise
+                [testing.main_unit_tests] (unknown function):0 Bitwise
+                [testing.main_unit_tests] (unknown function):0 Warn
+                [testing.main_unit_tests] (unknown function):0 Info
+                [testing.main_unit_tests] (unknown function):0 Debug
+                """ ),
+                output )
 
         else:
             frameinfo = getframeinfo(currentframe())
             line      = frameinfo.lineno
 
-            offset1 = -40
-            offset2 = -38
+            offset1 = -38
+            offset2 = -35
 
             self.assertEqual( wrap_text( """\
-                    [testing.main_unit_tests] test_function_name:{} Bitwise
-                    [testing.main_unit_tests] test_function_name:{} Bitwise
-                    [testing.main_unit_tests] test_function_name:{} Warn
-                    [testing.main_unit_tests] test_function_name:{} Info
-                    [testing.main_unit_tests] test_function_name:{} Debug
+                [testing.main_unit_tests] test_function_name:{} Bitwise
+                [testing.main_unit_tests] test_function_name:{} Bitwise
+                [testing.main_unit_tests] test_function_name:{} Warn
+                [testing.main_unit_tests] test_function_name:{} Info
+                [testing.main_unit_tests] test_function_name:{} Debug
 
-                    [testing.main_unit_tests] function_name:{} Bitwise
-                    [testing.main_unit_tests] function_name:{} Bitwise
-                    [testing.main_unit_tests] function_name:{} Warn
-                    [testing.main_unit_tests] function_name:{} Info
-                    [testing.main_unit_tests] function_name:{} Debug
-                    """.format(
-                            line+offset1+1, line+offset1+2, line+offset1+3, line+offset1+4, line+offset1+5,
-                            line+offset2+6, line+offset2+7, line+offset2+8, line+offset2+9, line+offset2+10,
-                    ) ), output )
+                [testing.main_unit_tests] function_name:{} Bitwise
+                [testing.main_unit_tests] function_name:{} Bitwise
+                [testing.main_unit_tests] function_name:{} Warn
+                [testing.main_unit_tests] function_name:{} Info
+                [testing.main_unit_tests] function_name:{} Debug
+                """.format(
+                        line+offset1+1, line+offset1+2, line+offset1+3, line+offset1+4, line+offset1+5,
+                        line+offset2+6, line+offset2+7, line+offset2+8, line+offset2+9, line+offset2+10,
+                ) ), output )
 
     def test_no_function_name_and_level(self):
+        stderr.clear()
+        log = getLogger( 127, "testing.main_unit_tests", date=True, function=False, level=True )
 
-        try:
-            sys.stderr = new_err
-            log = getLogger( 127, "testing.main_unit_tests", date=True, function=False, level=True )
+        log( 1, "Bitwise" )
+        log( 8, "Bitwise" )
+        log.warn( "Warn" )
+        log.info( "Info" )
+        log.debug( "Debug" )
 
-            log( 1, "Bitwise" )
-            log( 8, "Bitwise" )
-            log.warn( "Warn" )
-            log.info( "Info" )
-            log.debug( "Debug" )
-            log.reset()
+        log.newline()
+        log.reset()
 
-        finally:
-            sys.stderr = old_err
-
-        output = self.getOutput( "[testing.main_unit_tests] ", "2018-01-28 20:29:26:617.002010 3.63e-04 " )
+        output = stderr.contents( "[testing.main_unit_tests] ", "2018-01-28 20:29:26:617.002010 3.63e-04 " )
         self.assertEqual( wrap_text( """\
-                [testing.main_unit_tests] DEBUG(1) Bitwise
-                [testing.main_unit_tests] DEBUG(8) Bitwise
-                [testing.main_unit_tests] WARNING Warn
-                [testing.main_unit_tests] INFO Info
-                [testing.main_unit_tests] DEBUG Debug
-                """ ),
-                output )
+            [testing.main_unit_tests] DEBUG(1) Bitwise
+            [testing.main_unit_tests] DEBUG(8) Bitwise
+            [testing.main_unit_tests] WARNING Warn
+            [testing.main_unit_tests] INFO Info
+            [testing.main_unit_tests] DEBUG Debug
+            """ ),
+            output )
 
     def test_date_disabled(self):
+        stderr.clear()
+        log = getLogger( "testing.main_unit_tests", 127, date=False, function=False )
 
-        try:
-            sys.stderr = new_err
-            log = getLogger( "testing.main_unit_tests", 127, date=False, function=False )
+        log( 1, "Bitwise" )
+        log( 8, "Bitwise" )
+        log.warn( "Warn" )
+        log.info( "Info" )
+        log.debug( "Debug" )
 
-            log( 1, "Bitwise" )
-            log( 8, "Bitwise" )
-            log.warn( "Warn" )
-            log.info( "Info" )
-            log.debug( "Debug" )
-            log.reset()
+        log.newline()
+        log.reset()
 
-        finally:
-            sys.stderr = old_err
-
-        output = self.getOutput( "[testing.main_unit_tests] ", "20:29:26:617.002010 3.63e-04 " )
+        output = stderr.contents( "[testing.main_unit_tests] ", "20:29:26:617.002010 3.63e-04 " )
         self.assertEqual( wrap_text( """\
-                [testing.main_unit_tests] Bitwise
-                [testing.main_unit_tests] Bitwise
-                [testing.main_unit_tests] Warn
-                [testing.main_unit_tests] Info
-                [testing.main_unit_tests] Debug
-                """ ),
-                output )
+            [testing.main_unit_tests] Bitwise
+            [testing.main_unit_tests] Bitwise
+            [testing.main_unit_tests] Warn
+            [testing.main_unit_tests] Info
+            [testing.main_unit_tests] Debug
+            """ ),
+            output )
 
     def test_get_logger_empty(self):
+        stderr.clear()
+        log = getLogger( function=False )
 
-        try:
-            sys.stderr = new_err
-            log = getLogger( function=False )
+        log( 1, "Bitwise" )
+        log( 8, "Bitwise" )
+        log.warn( "Warn" )
+        log.info( "Info" )
+        log.debug( "Debug" )
 
-            log( 1, "Bitwise" )
-            log( 8, "Bitwise" )
-            log.warn( "Warn" )
-            log.info( "Info" )
-            log.debug( "Debug" )
-            log.reset()
+        log.newline()
+        log.reset()
 
-        finally:
-            sys.stderr = old_err
-
-        output = self.getOutput( "[logger.py] ", "20:29:26:617.002010 3.63e-04 " )
+        output = stderr.contents( "[logger.py] ", "20:29:26:617.002010 3.63e-04 " )
         self.assertEqual( wrap_text( """\
-                [logger.py] Bitwise
-                [logger.py] Bitwise
-                [logger.py] Warn
-                [logger.py] Info
-                [logger.py] Debug
-                """ ),
-                output )
+            [logger.py] Bitwise
+            [logger.py] Bitwise
+            [logger.py] Warn
+            [logger.py] Info
+            [logger.py] Debug
+            """ ),
+            output )
 
