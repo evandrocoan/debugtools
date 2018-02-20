d2a058590eedcb4dfc288b0b8f1245e8c514c146
evandrocoan <evandrocoan@hotmail.com>
2018-02-20 09:57:19 -0300

    Set verbosity=2 on tests/run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -10,8 +10,10 @@ PACKAGE_ROOT_DIRECTORY = os.path.dirname( os.path.realpath( __file__ ) )
 loader = unittest.TestLoader()
 start_dir = os.path.join( PACKAGE_ROOT_DIRECTORY, 'testing' )
 
+# What numbers can you pass as verbosity in running Python Unit Test Suites?
+# https://stackoverflow.com/questions/1322575/what-numbers-can-you-pass-as-verbosity-in-running-python-unit-test-suites
 suite = loader.discover( start_dir, "*unit_tests.py" )
-runner = unittest.TextTestRunner()
+runner = unittest.TextTestRunner(verbosity=2)
 
 # print( sys.path )
 results = runner.run( suite )
