c5052b2441f66cb9ad986f793eb18437d10c6d13
evandrocoan <evandrocoan@hotmail.com>
2018-02-24 00:10:23 -0300

    Moved manual_tests.py from tests/testing/ to tests/

--- tests/manual_tests.py
+++ tests/manual_tests.py
@@ -2,7 +2,7 @@
 
 try:
     # To run this file, run on the Sublime Text console:
-    # import imp; import DebugTools.tests.testing.manual_tests; imp.reload( DebugTools.tests.testing.manual_tests )
+    # import imp; import DebugTools.tests.manual_tests; imp.reload( DebugTools.tests.manual_tests )
     import sublime_plugin
 
 except (ImportError):
