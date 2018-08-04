d68637143035813bd5a44baa7c641fb6c86f2c49
evandrocoan <evandrocoan@hotmail.com>
2018-08-04 19:38:52 -0300

    Updated the LICENSE.txt name endorsement policies

--- all/debug_tools/logger.py
+++ all/debug_tools/logger.py
@@ -9,6 +9,20 @@
 #   Originally written on:
 #   https://github.com/evandrocoan/SublimeAMXX_Editor/blob/888c6822047d84e2370348b6cf5f4ac509f77b32/AMXXEditor.py#L1741-L1804
 #
+#  Redistributions of source code must retain the above
+#  copyright notice, this list of conditions and the
+#  following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials
+#  provided with the distribution.
+#
+#  Neither the name Evandro Coan nor the names of any
+#  contributors may be used to endorse or promote products
+#  derived from this software without specific prior written
+#  permission.
+#
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the
 #  Free Software Foundation; either version 3 of the License, or ( at
--- tests/testing/main_unit_tests.py
+++ tests/testing/main_unit_tests.py
@@ -6,6 +6,20 @@
 #   Copyright 2018 @ Evandro Coan
 #   Project Unit Tests
 #
+#  Redistributions of source code must retain the above
+#  copyright notice, this list of conditions and the
+#  following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials
+#  provided with the distribution.
+#
+#  Neither the name Evandro Coan nor the names of any
+#  contributors may be used to endorse or promote products
+#  derived from this software without specific prior written
+#  permission.
+#
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the
 #  Free Software Foundation; either version 3 of the License, or ( at
