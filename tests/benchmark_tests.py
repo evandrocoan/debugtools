5b012cce9b55170473660900d04e74e8b92adeec
evandrocoan <evandrocoan@hotmail.com>
2019-01-27 03:04:46 -0200

    Encapsulated the profiling runner into run_profiling function

--- tests/benchmark_tests.py
+++ tests/benchmark_tests.py
@@ -4,7 +4,7 @@
 ####################### Licensing #######################################################
 #
 #   Copyright 2019 @ Evandro Coan
-#   Helper functions and classes
+#   Benchmark this module versus logging and print methods
 #
 #  Redistributions of source code must retain the above
 #  copyright notice, this list of conditions and the
@@ -55,29 +55,40 @@ def average(stats, count):
 
     return stats
 
-def profile_something(run_profilling, count=1):
+def profile_something(profile_function, count=1):
     output_stream = StringIO()
-    profiller_status = pstats.Stats( stream=output_stream )
+    profiler_status = pstats.Stats( stream=output_stream )
 
     for index in range(count):
-        profiller = cProfile.Profile()
-        profiller.enable()
+        profiler = cProfile.Profile()
+        profiler.enable()
 
-        run_profilling()
-        profiller.disable()
-        profiller_status.add( profiller )
-        print( 'Profiled', '%.3f' % profiller_status.total_tt, 'seconds at', index, 'for', run_profilling.__name__, flush=True )
+        profile_function()
+        profiler.disable()
+        profiler_status.add( profiler )
+        print( 'Profiled', '%.3f' % profiler_status.total_tt, 'seconds at', index, 'for', profile_function.__name__, flush=True )
 
-    average(profiller_status, count)
-    profiller_status.sort_stats( "time" )
-    profiller_status.print_stats()
+    average(profiler_status, count)
+    profiler_status.sort_stats( "time" )
+    profiler_status.print_stats()
 
-    return "\nProfile results for %s\n%s" % ( run_profilling.__name__, output_stream.getvalue() ), profiller_status
+    return "\nProfile results for %s\n%s" % ( profile_function.__name__, output_stream.getvalue() ), profiler_status
 
-
+output = 1500
 log_debug_counts = 10
+
+def run_profiling(first_function, second_function):
+    first_function_results, first_function_stats = profile_something( first_function, log_debug_counts )
+    second_function_results, second_function_stats = profile_something( second_function, log_debug_counts )
+    difference = first_function_stats.total_tt - second_function_stats.total_tt
+
+    print( '\n', first_function_results[0:output] )
+    print( '\n', second_function_results[0:output] )
+    print( '\nTotal difference %.3f' % difference )
+
+
 log_debug_iterations = 1000000
-def debug_tools_module(iterations=log_debug_iterations):
+def debug_tools_log_debug_off(iterations=log_debug_iterations):
     log = logger.getLogger( 127, "benchmark", tick=False )
     log.setLevel( "DEBUG" )
     log.setLevel( "WARNING" )
@@ -86,7 +97,7 @@ def debug_tools_module(iterations=log_debug_iterations):
         # log( 1, 'Message' )
         log.debug( 'Message' )
 
-def logging_moduledule(iterations=log_debug_iterations):
+def logging_mod_log_debug_off(iterations=log_debug_iterations):
     date_format = "%H:%M:%S"
     string_format = "%(asctime)s:%(msecs)010.6f - %(name)s.%(funcName)s:%(lineno)d - %(message)s"
 
@@ -102,14 +113,6 @@ def logging_moduledule(iterations=log_debug_iterations):
     for index in range( iterations ):
         log.debug( 'Message' )
 
+run_profiling( debug_tools_log_debug_off, logging_mod_log_debug_off )
 
-debug_tools_results, debug_tools_stats = profile_something( debug_tools_module, log_debug_counts )
-logging_module_results, logging_module_stats = profile_something( logging_moduledule, log_debug_counts )
-
-output = 1500
-print( '\n', debug_tools_results[0:output] )
-print( '\n', logging_module_results[0:output] )
-
-difference = debug_tools_stats.total_tt - logging_module_stats.total_tt
-print( '\nTotal difference %.3f' % difference )
 
