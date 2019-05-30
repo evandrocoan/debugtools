34c8f0c08edce2721667d1b65f53729aa8f064c5
evandrocoan <evandrocoan@hotmail.com>
2019-05-29 23:41:44 -0300

    Added documentation to setup.py __version__ usage

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ except ImportError:
     import codecs
 
     try:
+        # https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
         filepath = 'all/debug_tools/version.py'
 
         with open( filepath, 'r' ) as file:
